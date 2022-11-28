f53b9e9959e272086c897a256a1a3bba5115ad70
noor <noorbarham690@gmail.com>
2022-11-28 16:42:46 +0200

    update dashboard.html and dashboard view

--- social/dwitter/urls.py
+++ social/dwitter/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from .views import dashboard,profile_list,profile
 
 app_name = "dwitter"
@@ -7,4 +7,5 @@ urlpatterns = [
     path("",dashboard, name="dashboard"),
     path("profile_list/", profile_list, name="profile_list"),
     path("profile/<int:pk>", profile, name="profile"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
